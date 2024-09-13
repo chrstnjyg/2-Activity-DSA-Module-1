6bc5213b8461f00fd69569009207b0d2d4dc8ebf
Christine <guerrerochristinejoy15@gmail.com>
2024-09-13 23:25:09 +0800

    Initial commit

---
+++ variables.js
@@ -0,0 +1,8 @@
+let name = "Christine Joy";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Female";
+let twitterHandle = "@chrstnejyyy";
+console.log("Name:", name);
+console.log("What do you want to become in your life:", whatDoYouWannaBecomeInYourLife);
+console.log("Gender:", gender);
+console.log("Twitter Handle:", twitterHandle);
